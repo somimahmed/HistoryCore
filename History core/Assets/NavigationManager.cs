f91f055498a875e3f0def257358d4b9d7f832c18
oxomgamedev <204450765+slimshady-kh@users.noreply.github.com>
2025-03-24 19:26:39 +0530

    Ui set

---
+++ History core/Assets/NavigationManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using DG.Tweening;
+
+
+public class NavigationManager : MonoBehaviour
+{ 
+    public GameObject settingPanel;
+
+
+    public void OpenSettingPanel(){
+    settingPanel.gameObject.SetActive(true);
+    settingPanel.GetComponent<RectTransform>().DOAnchorPos( Vector2.zero,.3f);   
+  } 
+
+  public void DeactivatingSettingPanel(){
+     Invoke("falsingSettingPanel",.3f); 
+    settingPanel.GetComponent<RectTransform>().DOAnchorPos(new Vector2(3000,0),.3f);
+  }
+  void falsingSettingPanel(){
+   settingPanel.gameObject.SetActive(true);
+  }
+
+
+}
