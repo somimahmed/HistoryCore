69de9dcbaea91f2e28ae7936d947737eab192d67
Ден Таг <dentag.com@gmail.com>
2025-04-30 22:17:30 +0500

    buttons added

--- History core/Assets/Scripts/cameralook.cs
+++ History core/Assets/Scripts/cameralook.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class cameralook : MonoBehaviour
 {
+    public Camera samera;
     public Vector3 targetRotation = new Vector3(18.4f, 181.0f, 0); // Rotation you want to reach
     public float rotationSpeed = 5f;                       // How fast it rotates
 
@@ -24,12 +25,12 @@ public class cameralook : MonoBehaviour
 
         if (shouldRotate)
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+            samera.transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
 
             // Stop rotating when close enough
             if (Quaternion.Angle(transform.rotation, desiredRotation) < 0.1f)
             {
-                transform.rotation = desiredRotation;
+                samera.transform.rotation = desiredRotation;
                 shouldRotate = false;
             }
         }
