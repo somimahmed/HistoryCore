23cf1f1de630b9bfeabfa13f16d19630e3552b3b
Ден Таг <dentag.com@gmail.com>
2025-05-03 04:23:46 +0500

    denis

---
+++ History core/Assets/Scripts/cameragooo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class cameragooo : MonoBehaviour
+{
+    public Transform h;
+    public Vector3 vector3;
+    void Start()
+    {
+        h.position=vector3;
+    }
+
+    
+}
